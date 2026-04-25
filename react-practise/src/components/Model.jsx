0aef29514f38f96a348f957ec8b629a219a12bb2
Muzammil Ali <muzammilali5994@outlook.com>
2026-04-25 23:57:54 +0500

    practise

--- react-practise/src/components/Model.jsx
+++ react-practise/src/components/Model.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
+
 function Model() {
   return(
     <div style={{width:'300px', height:'300px'}}>
+      <h1>3D Model</h1>
       <Canvas camera={{position: [5, 5, 5]}}>
         <mesh>
           <boxGeometry args={[3, 3, 3]} />
