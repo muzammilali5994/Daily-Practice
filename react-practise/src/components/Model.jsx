d2747d12625415e5c6f99450285126dc6812df7c
Muzammil Ali <muzammilali5994@outlook.com>
2026-05-03 21:53:33 +0500

    practise

--- react-practise/src/components/Model.jsx
+++ react-practise/src/components/Model.jsx
@@ -1,26 +1,14 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import React , {useState} from 'react'
+
 
 
 function Model() {
+  const [count, setCount] = useState(0)
   return(
-    <div style={{width:'300px', height:'300px'}}>
-      <h1>3D Model</h1>
-      <h1>asdf</h1>
-        <h1>3D Model</h1>
-      <h1>asdf</h1>
-            <h1>asdf</h1>
-        <h1>3D Model</h1>
-      <h1>asdf</h1>
-      <Canvas camera={{position: [5, 5, 5]}}>
-        <mesh>
-          <boxGeometry args={[3, 3, 3]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-        <ambientLight intensity={1} />
-        <OrbitControls enableZoom={true} />
-      </Canvas>
+    <div>
+      <h2>Count: {count}</h2>
+        <button onClick={()=>setCount(count + 1)}>+</button>
+        <button onClick={()=>setCount(count - 1)}>-</button>
     </div>
   )
 
